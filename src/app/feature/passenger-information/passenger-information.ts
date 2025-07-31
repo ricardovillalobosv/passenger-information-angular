68e8d8b06a4515514e99969654416ca3fa27cfb5
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-07-31 01:03:24 -0500

    Change passenger-information

--- src/app/feature/passenger-information/passenger-information.ts
+++ src/app/feature/passenger-information/passenger-information.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { Input } from '../../components/input/input';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
+export interface ItemForm {
+  id: FormControl<number>;
+  name: FormControl<string>;
+  value: FormControl<number>;
+}
+
+export type CustomFormGroup = FormGroup<ItemForm>;
 
 @Component({
   selector: 'app-passenger-information',
-  imports: [],
+  imports: [ReactiveFormsModule, Input, JsonPipe],
   templateUrl: './passenger-information.html',
-  styleUrl: './passenger-information.scss'
+  styleUrl: './passenger-information.scss',
 })
 export class PassengerInformation {
+  fb = inject(NonNullableFormBuilder);
 
+  form: FormGroup<{ items: FormArray<CustomFormGroup> }> = this.fb.group({
+    items: this.fb.array<CustomFormGroup>([]),
+  });
+
+  get items() {
+    return this.form.controls.items;
+  }
+
+  addItem() {
+    const id = this.items.length + 1;
+    const itemForm = this.fb.group<ItemForm>({
+      id: this.fb.control(id),
+      name: this.fb.control('', { validators: [Validators.required] }),
+      value: this.fb.control(0, { validators: [Validators.required] }),
+    });
+
+    this.form.controls.items.push(itemForm);
+  }
 }
