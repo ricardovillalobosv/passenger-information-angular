cce776e471525e610bbbcb160b0e52eb693dafcd
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2025-08-04 13:06:50 -0500

    Add select component

---
+++ src/app/components/select/select.ts
@@ -0,0 +1,56 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  forwardRef,
+  input,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+export interface SelectOptions {
+  id: number;
+  label: string;
+  value: string;
+}
+
+@Component({
+  selector: 'app-select',
+  imports: [ReactiveFormsModule],
+  templateUrl: './select.html',
+  styleUrl: './select.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => Select),
+      multi: true,
+    },
+  ],
+})
+export class Select implements ControlValueAccessor {
+  control = input.required<FormControl<any>>();
+  label = input.required<string>();
+  options = input.required<SelectOptions[]>();
+
+  onTouched = () => {};
+  onChange = (_value: any) => {};
+
+  writeValue(value: any): void {
+    if (value !== this.control().value) {
+      this.control().setValue(value, { emitEvent: false });
+    }
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.control().disable() : this.control().enable();
+  }
+}
